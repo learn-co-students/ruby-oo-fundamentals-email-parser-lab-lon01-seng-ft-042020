b45a5c36f66e3dcc6d3edf78747472cde817772b
ewelinaszoda <ewelinaszoda@gmail.com>
2020-04-23 17:12:12 +0200

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -22,7 +22,7 @@ end
 
 #binding.pry
 
-    # def parse   @email_addresses.split(/ |, |,/).uniq
+    # def parse 
     #     @email_addresses.split.map {|email_address|
     #     email_address.split(",")}.flatten.uniq  
     # end 
